ceb8017a5ea82f4a747b02adcdbf31336b1a3d29
Endriw98 <109055688+Endriw98@users.noreply.github.com>
2024-03-30 22:38:55 -0300

    Add files via upload

---
+++ js/msg.js
@@ -0,0 +1,7 @@
+function pegarValorInput() {
+    // Obtém o valor do input
+    var valor = document.getElementById('input').value;
+    
+    // Exibe o valor em um alerta (você pode fazer qualquer outra coisa com o valor)
+    alert('O valor do input é: ' + valor);
+}
